a4474c703afa3638ae4c611438b97f5efdbcdc9a
csy <505007945@qq.com>
2023-10-07 19:47:29 +0800

    feat: switch to use unshare
    1. use unshare in the child process instead of dicrectly specify the name space in clone syscall
    
    pitfall: if you want to use `CLONE_PARENT` then waitpid should be called by the parent of current process instead of calling by itself

--- lib/src/Cmd/Cmd.cpp
+++ lib/src/Cmd/Cmd.cpp
@@ -1,3 +1,4 @@
+#include <err.h>
 #include <iostream>
 #include <mini_container/Cmd/Cmd.hpp>
 #include <sys/mman.h>
@@ -8,14 +9,17 @@
 Exception<std::monostate> RunCmd::run() noexcept {
     char *stack = (char *)mmap(NULL, RunCmd::stackSize, PROT_READ | PROT_WRITE,
                                MAP_PRIVATE | MAP_ANONYMOUS | MAP_STACK, -1, 0);
-    pid_t pid = clone(setAndRun, stack + RunCmd::stackSize,
-                      CLONE_NEWUTS | SIGCHLD, &args[0]);
+    pid_t pid = clone(setAndRun, stack + RunCmd::stackSize, SIGCHLD, &args[0]);
+    if (pid == -1)
+        err(EXIT_FAILURE, "clone");
     waitpid(pid, NULL, 0);
     return std::monostate{};
 }
 
 int RunCmd::setAndRun(void *args) {
     char **innerArg = (char **)args;
+    unshare(CLONE_NEWUTS);
+    sethostname("root", 4);
     execve(innerArg[0], innerArg, NULL);
     return 0;
 }
