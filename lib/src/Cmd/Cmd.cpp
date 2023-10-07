3e82b8c482480254ff2b7b88ed6b96521e5540af
csy <505007945@qq.com>
2023-10-07 20:28:31 +0800

    feat: adding CLONE_NS
    this means now container has a new mount namespace, from now, the child process can change its root to a arbitrary mount point
    
    pitfall: since root has been changed, the executable path should also be changed

--- lib/src/Cmd/Cmd.cpp
+++ lib/src/Cmd/Cmd.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <mini_container/Cmd/Cmd.hpp>
 #include <sys/mman.h>
+#include <sys/mount.h>
+#include <sys/stat.h>
 #include <sys/syscall.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -18,7 +20,29 @@ Exception<std::monostate> RunCmd::run() noexcept {
 
 int RunCmd::setAndRun(void *args) {
     char **innerArg = (char **)args;
-    unshare(CLONE_NEWUTS);
+    unshare(CLONE_NEWUTS | CLONE_NEWNS);
+    const char *new_root = "/home/csy/container/fake_root";
+    const char *put_old = "/oldrootfs";
+    char path[10000];
+    if (mount(NULL, "/", NULL, MS_REC | MS_PRIVATE, NULL) == -1)
+        err(EXIT_FAILURE, "mount-MS_PRIVATE");
+
+    if (mount(new_root, new_root, NULL, MS_BIND, NULL) == -1)
+        err(EXIT_FAILURE, "mount-MS_BIND");
+
+    snprintf(path, sizeof(path), "%s/%s", new_root, put_old);
+    if (mkdir(path, 0777) == -1)
+        err(EXIT_FAILURE, "mkdir");
+    if (syscall(SYS_pivot_root, new_root, path) == -1)
+        err(EXIT_FAILURE, "pivot_root");
+
+    if (chdir("/") == -1)
+        err(EXIT_FAILURE, "chdir");
+
+    if (umount2(put_old, MNT_DETACH) == -1)
+        perror("umount2");
+    if (rmdir(put_old) == -1)
+        perror("rmdir");
     sethostname("root", 4);
     execve(innerArg[0], innerArg, NULL);
     return 0;
